e768418c55091f8f93b87fa6538fe05bab9951fe
Ian Wiseman <68253726+ian-wiseman@users.noreply.github.com>
2021-06-25 00:13:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,19 @@ const port = process.env.PORT || 4000;
 
 //middleware
 app.use(cors());
-app.use(express.json)
+app.use(express.json);
 
 // db config
-const MongoURI = ''
+const MongoURI = '';
 
 mongoose.connect(MongoURI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
 
 mongoose.connection.once('open', () => {
-    console.log('DB Connected')
+    console.log('DB Connected');
 
 })
 
@@ -30,7 +30,7 @@ mongoose.connection.once('open', () => {
 app.get('/', (req, res) => res.status(200).send('Welcome to Teale Chat'));
 
 app.post('/new/channel', (req, res) => {
-    const dbData = req.body
+    const dbData = req.body;
 
     mongoData.create(dbData, (err, data) => {
         if (err) {
@@ -50,9 +50,9 @@ app.post('/new/message', (req, res) => {
         { $push: { conversation: newMessage }},
     (err, data) => {
         if (err) {
-            res.send(500).send(err)
+            res.send(500).send(err);
         } else {
-            res.send(201).send(data)
+            res.send(201).send(data);
         }
     })    
     
@@ -61,33 +61,33 @@ app.post('/new/message', (req, res) => {
 app.get('/get/channelList', (req, res) => {
     mongoData.find((err, data) => {
         if (err) {
-            res.send(500).send(err)
+            res.send(500).send(err);
         } else {
-            let channels = []
+            let channels = [];
             data.map((channelData) => {
                 const channelInfo = {
                     id: channelData._id,
                     name: channelData.channelName
                 }
-                channels.push(channelInfo)
+                channels.push(channelInfo);
             })
 
-            res.status(200).send(channels)
+            res.status(200).send(channels);
         }
     })
 })
 
 app.get('/get/conversation', (req, res) => {
-    const id = req.query.id
+    const id = req.query.id;
 
     mongoData.find({ _id: id }, (err, data) => {
         if (err) {
-            res.status(500).send(err)
+            res.status(500).send(err);
         } else {
-            res.status(200).send(data)
+            res.status(200).send(data);
         }
     })
 })
 
 // listen
-app.listen(port, () => console.log(`Listening on localhost:${port}`))
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
