c8a37cc1d36a165e136f9de66be2add9ade4b6ad
Ian Wiseman <68253726+ian-wiseman@users.noreply.github.com>
2021-06-30 07:40:59 -0400

    Create validation.js

---
+++ validation.js
@@ -0,0 +1,35 @@
+const joi = require('@hapi/joi')
+
+// Register validation
+const registerValidation = (data) => {
+  const schema = {
+    name: joi.string()
+        .min(6)
+        .required(),
+    email: joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password: joi.string()
+        .min(6)
+        .required()
+  };
+  return joi.validate(data, schema);
+}
+
+// Login validation
+const loginValidation = (data) => {
+  const schema = {
+    email: joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password: joi.string()
+        .min(6)
+        .required()
+  };
+  return joi.validate(data, schema);
+}
+
+model.exports.registerValidation = registerValidation;
+model.exports.registerValidation = loginValidation;
