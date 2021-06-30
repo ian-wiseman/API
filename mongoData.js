b434a0773e32ee73f9b65d91c1636954ec8f61b0
Ian Wiseman <68253726+ian-wiseman@users.noreply.github.com>
2021-06-30 06:39:04 -0400

    Update mongoData.js

--- mongoData.js
+++ mongoData.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const slackSchema = mongoose.Schema({
+const conversationSchema = mongoose.Schema({
     channelName: String,
     conversation: [
         {
@@ -9,6 +9,20 @@ const slackSchema = mongoose.Schema({
             userName: String
         }
     ]
-})
+});
 
-export default mongoose.model('conversations', slackSchema)
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 5
+    },
+    email: {
+        type: String,
+        required: true,
+        min: 7
+    }
+});
+
+export default mongoose.model('conversations', conversationSchema)
+export default mongoose.model('user', userSchema)
